b6a3b97eb8901570aa6fde2ac71c38259ee91b4b
Frank <frank@void0.ch>
2023-02-22 21:05:36 +0100

    Fix db server

--- app/db.server.ts
+++ app/db.server.ts
@@ -22,26 +22,7 @@ if (process.env.NODE_ENV === "production") {
 
 function getClient() {
   const { DATABASE_URL } = process.env;
-  invariant(typeof DATABASE_URL === "string", "DATABASE_URL env var not set");
-
-  const databaseUrl = new URL(DATABASE_URL);
-
-  const isLocalHost = databaseUrl.hostname === "localhost";
-
-  const PRIMARY_REGION = isLocalHost ? null : process.env.PRIMARY_REGION;
-  const FLY_REGION = isLocalHost ? null : process.env.FLY_REGION;
-
-  const isReadReplicaRegion = !PRIMARY_REGION || PRIMARY_REGION === FLY_REGION;
-
-  if (!isLocalHost) {
-    databaseUrl.host = `${FLY_REGION}.${databaseUrl.host}`;
-    if (!isReadReplicaRegion) {
-      // 5433 is the read-replica port
-      databaseUrl.port = "5433";
-    }
-  }
-
-  console.log(`🔌 setting up prisma client to ${databaseUrl.host}`);
+  console.log(`🔌 setting up prisma client to hey ${DATABASE_URL}`);
   // NOTE: during development if you change anything in this function, remember
   // that this only runs once per server restart and won't automatically be
   // re-run per request like everything else is. So if you need to change
@@ -49,7 +30,7 @@ function getClient() {
   const client = new PrismaClient({
     datasources: {
       db: {
-        url: databaseUrl.toString(),
+        url: DATABASE_URL,
       },
     },
   });
