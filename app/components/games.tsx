fd2311e3a9f93d33924b459a06bdaa300940a9ff
Frank <frank@void0.ch>
2023-02-25 22:44:35 +0100

    Add games

--- app/components/games.tsx
+++ app/components/games.tsx
@@ -1,16 +1,59 @@
 import { prisma } from "~/db.server";
 import { json } from "@remix-run/node";
-import { useLoaderData } from "@remix-run/react";
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Heading,
+  SimpleGrid,
+  Center, Editable, EditablePreview, EditableInput
+} from "@chakra-ui/react";
+import type { Game, Player } from "@prisma/client";
 
 export async function loader() {
-  const games = await prisma.game.findMany();
+  const games = await prisma.game.findMany({ include: { player1: true, player2: true } });
   return json({ games });
 }
 
-export default function Games() {
-  const data = useLoaderData<typeof loader>();
+// maybe I should use https://remix.run/docs/en/v1/hooks/use-fetcher
+async function updateGame(value: string, game: Game) {
+  await prisma.game.update({ where: { id: game.id }, data: { ...game } });
+}
 
-  return (<>
-    { data.games.map(g => (<>g.player1</>)) }
-  </>)
+type AllGamesProps = {
+  games: (Game & { player1: Player, player2: Player })[]
+}
+
+export default function AllGames(props: AllGamesProps) {
+  return <div>
+    <SimpleGrid spacing={4} templateColumns="repeat(auto-fill, minmax(200px, 1fr))">
+      {props.games.map(game => (
+          <Card key={game.id}>
+            <CardHeader>
+              <Heading size="md">
+                <Center>{game.player1.name} vs {game.player2.name}</Center>
+              </Heading>
+            </CardHeader>
+            <CardBody>
+              <Center>
+                <Editable defaultValue={game.scorePlayer1.toString()}
+                          onSubmit={value => updateGame(value, game)}>
+                  <EditablePreview />
+                  <EditableInput />
+                </Editable>
+                :
+                <Editable defaultValue={game.scorePlayer2.toString()}>
+                  <EditablePreview />
+                  <EditableInput />
+                </Editable>
+              </Center>
+            </CardBody>
+            <CardFooter>
+            </CardFooter>
+          </Card>
+        )
+      )}
+    </SimpleGrid>
+  </div>;
 }
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,9 +1,20 @@
 import Games from "~/components/games";
+import { prisma } from "~/db.server";
+import { json } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+import { Game } from "@prisma/client";
+
+export async function loader() {
+  const games = await prisma.game.findMany({ include: { player1: true, player2: true } });
+  return json({ games });
+}
 
 export default function Index() {
+  const data = useLoaderData<typeof loader>();
+
   return (
     <>
-   <Games></Games>
+      <Games games={data.games}></Games>
     </>
   );
 }
