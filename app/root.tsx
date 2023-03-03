5a2bbb64c7f9f36204f7cc80e22dc395c189801c
Frank <frank@void0.ch>
2023-03-03 22:21:47 +0100

    Fix title

--- app/root.tsx
+++ app/root.tsx
@@ -44,7 +44,7 @@ const Document = withEmotionCache(({ children, title }: DocumentProps, emotionCa
       <meta charSet="utf-8" />
       <meta name="viewport" content="width=device-width,initial-scale=1" />
       <meta name="theme-color" content={theme.palette.primary.main} />
-      {title ? <title>{title}</title> : null}
+      <title>Ping Pong Tournament</title>
       <Meta />
       <Links />
       <link
