7959129ac8b738afd62b0106d6eafd85e2256711
Frank <frank@void0.ch>
2023-03-03 19:29:21 +0100

    many things

--- app/routes/tournaments/$tournamentName.tsx
+++ app/routes/tournaments/$tournamentName.tsx
@@ -7,28 +7,28 @@ import {
   Card,
   CardActions,
   CardContent,
-  CardMedia, Input,
+  CardMedia, Grid, Input,
   Modal,
   Tab,
   Tabs,
   Typography
 } from "@mui/material";
-import type { SyntheticEvent} from "react";
+import type { SyntheticEvent } from "react";
 import React, { useState } from "react";
 import type { Game } from ".prisma/client";
 import type { Player } from "@prisma/client";
 
 
 const style = {
-  position: 'absolute' as 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
+  position: "absolute" as "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
   width: 400,
-  bgcolor: 'background.paper',
-  border: '2px solid #000',
+  bgcolor: "background.paper",
+  border: "2px solid #000",
   boxShadow: 24,
-  p: 4,
+  p: 4
 };
 
 export async function loader({ params }: LoaderArgs) {
@@ -42,18 +42,16 @@ export async function loader({ params }: LoaderArgs) {
 
 export async function action({ request }: ActionArgs) {
   const form = await request.formData();
-  console.log("bla", form);
 
   // Use zod to validate data
   const scorePlayer1 = parseInt(form.get("scorePlayer1") as string);
   const scorePlayer2 = parseInt(form.get("scorePlayer2") as string);
-  const game = form.get("gameId") as string;
+  const gameId = parseInt(form.get("gameId") as string);
 
   try {
-    await prisma.game.update({ where: { id: game }, data: { scorePlayer1, scorePlayer2 } });
+    await prisma.game.update({ where: { id: gameId }, data: { scorePlayer1, scorePlayer2 } });
     return null;
   } catch (error) {
-    console.log("frank", error);
     return null;
   }
 }
@@ -64,13 +62,13 @@ interface TabPanelProps {
   value: number;
 }
 
-function EditModal({ game }: { game:  Game & {player1: Player, player2: Player}}) {
+function EditModal({ game }: { game: Game & { player1: Player, player2: Player } }) {
   const [open, setOpen] = useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = (event: any) => {
     submit(event.currentTarget, { replace: true });
     setOpen(false);
-  }
+  };
   const submit = useSubmit();
 
   return (
@@ -83,18 +81,20 @@ function EditModal({ game }: { game:  Game & {player1: Player, player2: Player}}
         aria-describedby="modal-modal-description"
       >
         <Form method="post">
-        <Box sx={style}>
-          <Typography id="modal-modal-title" variant="h6" component="h2">
-            Edit score
-          </Typography>
-          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-            { game.player1.name }: <Input name="scorePlayer1" placeholder={game.scorePlayer1.toString()}></Input>
-            <br />
-            { game.player2.name }: <Input name="scorePlayer2" placeholder={game.scorePlayer2.toString()}></Input>
-            <Input type="hidden" name="gameId" value={game.id}></Input>
-          </Typography>
-          <Button onClick={handleClose}>Submit</Button>
-        </Box>
+          <Box sx={style}>
+            <Typography id="modal-modal-title" variant="h6" component="h2">
+              Edit score
+            </Typography>
+            <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+              {game.player1.name}: <Input name="scorePlayer1"
+                                          placeholder={game.scorePlayer1.toString()}></Input>
+              <br />
+              {game.player2.name}: <Input name="scorePlayer2"
+                                          placeholder={game.scorePlayer2.toString()}></Input>
+              <Input type="hidden" name="gameId" value={game.id}></Input>
+            </Typography>
+            <Button onClick={handleClose}>Submit</Button>
+          </Box>
         </Form>
       </Modal>
     </div>
@@ -137,43 +137,45 @@ export default function Tournament() {
   };
 
   return (
-    <Box sx={{ width: '100%' }}>
-      <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+    <Grid sx={{ width: "100%" }}>
+      <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
         <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
           <Tab label="Games" {...a11yProps(0)} />
-          <Tab label="Stats" {...a11yProps(1)} />
+          <Tab label="Live Scoreboard" {...a11yProps(1)} />
         </Tabs>
       </Box>
       <TabPanel value={value} index={0}>
-        <Box sx={{ display: 'flex' }}>
-        {data.tournament?.games.map(game => (
-          <Card sx={{ maxWidth: 200 }} key={game.id}>
-            <CardMedia
-              sx={{ height: 50 }}
-              image="/static/images/cards/contemplative-reptile.jpg"
-              title="green iguana"
-            />
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="div">
-                {game.player1.name} vs {game.player2.name}
-              </Typography>
-              <Typography variant="body2" color="text.secondary">
-                {game.scorePlayer1.toString()}
-                :
-                {game.scorePlayer2.toString()}
-              </Typography>
-            </CardContent>
-            <CardActions>
-              <EditModal game={game}></EditModal>
-            </CardActions>
-          </Card>
-        ))}
-        </Box>
+        <Grid container spacing={2}>
+          <Grid item xs={4}>
+            {data.tournament?.games.map(game => (
+              <Card sx={{ maxWidth: 200, minWidth: 50 }} key={game.id.toString()}>
+                <CardMedia
+                  sx={{ height: 50 }}
+                  image="/static/images/cards/contemplative-reptile.jpg"
+                  title="green iguana"
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="div">
+                    {game.player1.name} vs {game.player2.name}
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    {game.scorePlayer1.toString()}
+                    :
+                    {game.scorePlayer2.toString()}
+                  </Typography>
+                </CardContent>
+                <CardActions>
+                  <EditModal game={game}></EditModal>
+                </CardActions>
+              </Card>
+            ))}
+          </Grid>
+        </Grid>
       </TabPanel>
       <TabPanel value={value} index={1}>
         Item Two
       </TabPanel>
-    </Box>
+    </Grid>
   );
 }
 
