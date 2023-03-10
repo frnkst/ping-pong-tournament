396bebc92bba562af792bb3f18f49221a7f0ee0b
Frank <frank@void0.ch>
2023-03-10 12:26:27 +0100

    Checkmark

--- app/routes/components/GameCard.tsx
+++ app/routes/components/GameCard.tsx
@@ -2,16 +2,21 @@ import { Card, CardActions, CardContent, Grid, Typography } from "@mui/material"
 import UpdateScore from "~/routes/components/UpdateScore";
 import React from "react";
 import type { Game, Player } from "@prisma/client";
+import CheckCircleIcon from "@mui/icons-material/CheckCircle";
 
-export type GameWithPlayer = Game & {player1: Player, player2: Player}
+export type GameWithPlayer = Game & { player1: Player, player2: Player }
 
 export default function GameCard({ game }: { game: GameWithPlayer }) {
+  const gameCompleted = (game.scorePlayer1 >= 21 || game.scorePlayer2 >= 21);
   return (<Grid item xs={6}>
     <Card>
       <CardContent>
-        <Typography variant="body2" color="text.secondary">
-          Game: {game.id}
-        </Typography>
+        <div style={{ display: "flex", justifyContent: "space-between" }}>
+          <Typography variant="body2" color="text.secondary">
+            Game: {game.id}
+          </Typography>
+          {gameCompleted && <><CheckCircleIcon style={{ color: "green" }}></CheckCircleIcon></>}
+        </div>
         <br />
         <Typography align="center">
           {game.player1.name} - {game.player2.name}
